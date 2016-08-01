36af8166d6068bbfccfc4e7f07afa5b3e3a66012
dc-artem <a.fedorchuk@delivery-club.ru>
2016-08-01 12:54:39 +0300

    Доработаны тесты из-за изменений в приложении

--- test_API/testapi.py
+++ test_API/testapi.py
@@ -1,86 +1,86 @@
 import requests
 import json
+import pytest
+
+class TestPersonalData:
 
 
-def test_info():
-    url = "http://personal-data-service.dev/customers/test@tets.ru"
-    r2 = requests.get(url)
-    print(r2)
-    print("\nSTATUS:", r2.status_code)
-    data_json = json.loads(r2.text)
-    print("RESPONSE:\n", data_json, "\n")
-    assert data_json['type'] == 1
+    def test_info(self):
+        url = "http://personal-data-service.dev/customers/test@tets.ru"
+
+        r2 = requests.get(url)
+        print("\nSTATUS:", r2.status_code)
+        data_json = json.loads(r2.text)
+        print("RESPONSE:\n", data_json, "\n")
+        assert data_json['type'] == 1
 
 
-def test_requests():
-    r = requests.get("http://personal-data-service.dev/stats")
-    print("\nSTATUS:", r.status_code)
-    data_json = json.loads(r.text)
-    print("RESPONSE:\n", data_json, "\n")
-    assert data_json['type'] == 1
+    def test_requests(self):
+        r = requests.get("http://personal-data-service.dev/stats")
+        print("\nSTATUS:", r.status_code)
+        data_json = json.loads(r.text)
+        print("RESPONSE:\n", data_json, "\n")
+        assert data_json['type'] == 1
 
 
-def test_delete_profile():
-    headers = {
-        'Content-type': 'application/json'
-    }
+    def test_email_info(self):
 
-    data = {
-        "deletion": {
-            "initiator": "current333@example.com",
-            "reason": "It's my desire"
+        url = 'http://personal-data-service.dev/customers/new11@example.com'
+        headers = {
+            'Content-type': 'application/json'
         }
-    }
+        data = {
+            "customerInfo": {
+                "email": "new11@example.com",
+                "firstName": "Olaf",
+                "lastName": "Henderson",
+                "birthDate": "1990-12-12",
+                "mobilePhoneCountryCode": "+49",
+                "mobilePhoneNumber": "123-44-66"
+            },
+            "customerAddress": {
+                "cityId": 123,
+                "areaId": 321,
+                "addressLine1": "Арбат",
+                "addressLine2": "Арбатская",
+                "addressLine3": "",
+                "addressLine4": "",
+                "addressLine5": "",
+                "addressOther": "",
+                "room": "1",
+                "flatNumber": "12",
+                "structure": "1F",
+                "building": "B",
+                "intercom": "111",
+                "entrance": "222",
+                "floor": "10",
+                "district": "Northern",
+                "postcode": "111222",
+                "latitude": "22.265033",
+                "longitude": "114.153"
+            }
+        }
+        response = requests.put(url, data=json.dumps(data), headers=headers)
+        print("RESP:", response, "\n")
+        json1 = json.loads(response.text)
+        print("JSON:\n", json1)
 
-    url = "http://personal-data-service.dev/customers/current333@example.com"
-    r = requests.delete(url, data=json.dumps(data), headers=headers)
-    print("\nSTATUS:", r.status_code)
-    data_json = json.loads(r.text)
-    print("RESPONSE:\n", data_json, "\n")
-    assert data_json['type'] == 1
 
-def test_email_info():
+    def test_delete_profile(self):
 
-    headers = {
-        'Content-type':'application/json'
-    }
-    data = {
-        "customer": {
-            "email": "current@example.com"
-        },
-        "customerInfo": {
-            "email": "current@example.com",
-            "firstName": "Olaf",
-            "lastName": "Henderson",
-            "birthDate": "1990-12-12",
-            "mobilePhoneCountryCode": "+49",
-            "mobilePhoneNumber": "123-44-66"
-        },
-        "customerAddress": {
-            "cityId": 123,
-            "areaId": 321,
-            "addressLine1": "Arbat",
-            "addressLine2": "Arbatskaya",
-            "addressLine3": "",
-            "addressLine4": "",
-            "addressLine5": "",
-            "addressOther": "",
-            "room": "1",
-            "flatNumber": "12",
-            "structure": "1F",
-            "building": "B",
-            "intercom": "111",
-            "entrance": "222",
-            "floor": "10",
-            "district": "Northern",
-            "postcode": "111222",
-            "latitude": "22.265033",
-            "longitude": "114.153"
+        url = "http://personal-data-service.dev/customers/new11@example.com"
+        headers = {
+            'Content-type': 'application/json'
         }
 
-    }
-    url = 'http://personal-data-service.dev/customers'
-    response = requests.put(url, data=json.dumps(data), headers=headers)
-    print("RESP:", response, "\n")
-    json1 = json.loads(response.text)
-    print("JSON:\n", json1)
+        data = {
+            "deletion": {
+                "initiator": "new11@example.com",
+                "reason": "It's my desire"
+            }
+        }
+        r = requests.delete(url, data=json.dumps(data), headers=headers)
+        print("\nSTATUS:", r.status_code)
+        data_json = json.loads(r.text)
+        print("RESPONSE:\n", data_json, "\n")
+        assert data_json['type'] == 1
