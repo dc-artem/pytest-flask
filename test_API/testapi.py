e536bb6d09464e74a2ae863d99e1d4ce4635b401
dc-artem <a.fedorchuk@delivery-club.ru>
2016-08-02 16:17:50 +0300

    тесты независимы друг от друга и доработаны

--- test_API/testapi.py
+++ test_API/testapi.py
@@ -1,15 +1,17 @@
 import requests
 import json
-import pytest
 import os.path
 from test_API.mail_random_generator import generate_random_email
+
 
 class TestPersonalData:
 
     email = generate_random_email(7)
-    config = "adduser.json"
+    emailfordeluser = generate_random_email(7)
+
+    config = "target.json"
     config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), config)
-    # with open('adduser.json', 'r', encoding='utf-8') as fh:
+    # with open('target.json', 'r', encoding='utf-8') as fh:
     with open(config_file) as fh:
         data = json.load(fh)
     headers = {
@@ -17,66 +19,87 @@ class TestPersonalData:
     }
     baseUrlCustomers = data['urls']["baseUrlCustomers"]
     baseUrl = data['urls']["baseUrl"]
-    emailjson = data['addNewUser']['customerInfo']['email']
-    print("\nemailjson:", emailjson)
-    emailjson = {'email': email}
-    print("\nemailjson:", emailjson)
-    print(email)
-
-
 
     def request_information_about_system(self):
         pass
-
-
-
 
     def test_insert_personal_data(self):
         addUserData = TestPersonalData.data['addNewUser']
         addUserData['customerInfo']['email'] = TestPersonalData.email
         data = json.dumps(addUserData)
-        print(data)
-
         response = requests.put(TestPersonalData.baseUrlCustomers + TestPersonalData.email,
                                 data=json.dumps(data),
                                 headers=TestPersonalData.headers)
-        print(response)
         data_json = json.loads(response.text)
 
         assert data_json['type'] == 1
         assert response.status_code == 200
 
-    def insert_or_update_personal_data(self):
+    def test_insert_and_update_personal_data(self):
+        """
+        Добавлем нового пользователя с емейлом current@example.com
+        """
         response = requests.put(TestPersonalData.baseUrlCustomers + 'current@example.com',
-                                data=json.dumps(TestPersonalData.data['addNewUser']), headers=TestPersonalData.headers)
+                                data=json.dumps(TestPersonalData.data['addUser']),
+                                headers=TestPersonalData.headers)
         data_json = json.loads(response.text)
 
         assert data_json['type'] == 1
         assert response.status_code == 200
 
-    def add_and_delete_profile(self):
+        """
+        Обновляем данные для пользователя с емейлом current@examle.com
+
+        """
         response = requests.put(TestPersonalData.baseUrlCustomers + 'current@example.com',
-                                data=json.dumps(TestPersonalData.data['userForDel']), headers=TestPersonalData.headers)
+                                data=json.dumps(TestPersonalData.data['editPersonalDate']),
+                                headers=TestPersonalData.headers)
+        data_json2 = json.loads(response.text)
+
+        assert data_json2['type'] == 1
+        assert response.status_code == 200
+
+    def test_add_and_delete_profile(self):
+        """
+        Добавление нового пользователя с уникальным емейлом
+        """
+
+        addUserData = TestPersonalData.data['userForDel']
+        addUserData['customerInfo']['email'] = TestPersonalData.emailfordeluser
+        data = json.dumps(addUserData)
+
+        response = requests.put(TestPersonalData.baseUrlCustomers + TestPersonalData.emailfordeluser,
+                                data=json.dumps(data),
+                                headers=TestPersonalData.headers)
         data_json = json.loads(response.text)
 
         assert data_json['type'] == 1
         assert response.status_code == 200
 
-        respD = requests.delete(TestPersonalData.baseUrlCustomers + 'testadduser11@example.com',
-                                data=json.dumps(TestPersonalData.data['forDelete']), headers=TestPersonalData.headers)
+        """
+        Удаление пользователя с уникальным емейлом
+        """
+        delUserData = TestPersonalData.data['forDelete']
+        delUserData['deletion']['initiator'] = TestPersonalData.emailfordeluser
+        datadel = json.dumps(delUserData)
+
+        respD = requests.delete(TestPersonalData.baseUrlCustomers + TestPersonalData.emailfordeluser,
+                                data=datadel, headers=TestPersonalData.headers)
 
         data_json = json.loads(respD.text)
-        assert data_json['type'] == 1
-        assert respD.status_code == 200
 
-    def getting_information_by_email(self):
-        response = requests.get(TestPersonalData.baseUrlCustomers + 'new11@example.com')
+        assert respD.status_code == 200
+        assert data_json['type'] == 1
+
+
+    def test_getting_information_by_email(self):
+        response = requests.get(TestPersonalData.baseUrlCustomers + TestPersonalData.email)
         data_json = json.loads(response.text)
         assert data_json['type'] == 1
         assert response.status_code == 200
 
-    def getting_statistics_on_personal_data(self):
-        response = requests.get(TestPersonalData.baseUrl +"stats")
+    def test_getting_statistics_on_personal_data(self):
+        response = requests.get(TestPersonalData.baseUrl + "stats")
         data_json = json.loads(response.text)
         assert data_json['type'] == 1
         assert response.status_code == 200
